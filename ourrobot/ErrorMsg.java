250998fde35058874facdfaa7e5292eae933d874
hadasB3 <32985754+hadasB3@users.noreply.github.com>
2018-02-08 15:22:42 +0200

    Add files via upload

---
+++ ourrobot/ErrorMsg.java
@@ -0,0 +1,13 @@
+package com.example.hadas.ourrobot;
+
+/**
+ * Created by
+ * Hadas Barel
+ * Channa Goldberg
+ * Meizy Gambash
+ * on 10/09/2017.
+ */
+
+public interface ErrorMsg {
+    public void setError(String str);
+}
